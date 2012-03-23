d78da75a094981205ba017d1b0ca6bb1d15b969a
Ramanujan Jan <esis_coder@libero.it>
2012-03-23 12:51:50 +0100

    Moquito initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moquito::Application.config.secret_token = 'e903f8bb6ec7c318abe9c9baf9a5b48beb1d3272011f56267d74b7605f98eeb0dd84bfea9b29dbcbb344a64f92fa3b8e6573742d1754c41cb50c01971b9fab81'
